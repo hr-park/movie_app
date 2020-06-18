c1390c6fc6f2c92a94741de1d22b1b7a05336248
hr-park <ever-iver@hanmail.net>
2020-06-18 17:49:53 +0900

    Updates

---
+++ precache-manifest.6355914eb321b2ddade39b0756a60613.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb0775001636db8a35b30e1435a84cee",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "cfa974355cf4cef4c4fd",
+    "url": "/movie_app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cc23e89feaa074d40019",
+    "url": "/movie_app/static/js/2.cbe4378b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.cbe4378b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfa974355cf4cef4c4fd",
+    "url": "/movie_app/static/js/main.a907063b.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/movie_app/static/media/logo.5d5d9eef.svg"
+  }
+]);
